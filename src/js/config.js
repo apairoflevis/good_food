f05f728d5af3c0c1039f2f366934cea042e88213
Andrew <delapaz10@gmail.com>
2019-03-01 02:58:40 -0600

    fixed api issue

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const proxy = 'https://cors-anywhere.herokuapp.com/';
-export const key = 'c55832b9fb3335fc755ee96b708bebd9';
+export const key = "c55832b9fb3335fc755ee96b708bebd9";
 
 //595e9c8bf483a30e2f1c4c3cedf318d8
 //4e8ab054558f9df85dee797659e608fa
