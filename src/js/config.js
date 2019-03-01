c0be81f5c96e1ba5ad573efbd839a905c49cc521
Andrew <delapaz10@gmail.com>
2019-03-01 02:11:20 -0600

    First Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = 'ef5bc86d0ac4f8110e8684da70715599';
+
+//595e9c8bf483a30e2f1c4c3cedf318d8
+//4e8ab054558f9df85dee797659e608fa
